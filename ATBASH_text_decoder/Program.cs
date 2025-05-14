3ee6d1d74d73556d730830c2b45a8b99720dd64e
Elazar-r <arye9890@gmail.com>
2025-05-14 15:56:54 +0300

    Function construction CalculatorWarning

--- ATBASH_text_decoder/Program.cs
+++ ATBASH_text_decoder/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+string CalculatorWarning(int pointNumber)
+{
+    string alertstring = "";
+    if (pointNumber == 0)
+    {
+        alertstring += "NO WAENING!";
+    }
+    else if (pointNumber >= 1 && pointNumber <= 5)
+    {
+        alertstring += "WARNING!!!";
+    }
+
+    else if (pointNumber >= 6 && pointNumber <= 10)
+    {
+        alertstring += "DANGER!!!";
+    }
+    else if (pointNumber >= 11 && pointNumber <= 15)
+    {
+        alertstring += "ULTRA ALERT!!!";
+    }
+    else
+    {
+        alertstring += "ERROR!!!";
+    }
+    return alertstring;
+}
